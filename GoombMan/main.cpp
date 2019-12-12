72cf1f3c18862d8dfa4f2ef0b48d316a5d1e3772
Adan Anwar <adananwar71@gmail.com>
2019-12-12 15:26:35 +0100

    Initial Commit

---
+++ GoombMan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoombMan
+//
+//  Created by Adan Anwar on 2019-12-12.
+//  Copyright © 2019 Adan Anwar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
