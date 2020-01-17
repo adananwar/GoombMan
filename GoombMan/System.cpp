7aeb06b1352bc15cb4ab3be3657bfd1ad79484b3
phylyps <maohgren@gmail.com>
2020-01-17 14:05:19 +0100

    sfdmo

--- GoombMan/System.cpp
+++ GoombMan/System.cpp
@@ -13,14 +13,16 @@ namespace GoombMan {
         SDL_Init(SDL_INIT_EVERYTHING);
         win = SDL_CreateWindow("GoombMan", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, 0);
         ren = SDL_CreateRenderer(win, -1, 0);
+        windowSurf = SDL_GetWindowSurface(win);
+
+        std::cout << "height: " << windowSurf->h << std::endl;
+
         TTF_Init();
         font = TTF_OpenFont("Roboto-Regular.ttf", 36);
 
         SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
         SDL_RenderClear(ren);
         SDL_RenderPresent(ren);
-
-        SDL_Delay(300);
 
     }
 
